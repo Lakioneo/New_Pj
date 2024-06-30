369293edd5b0ca279417b4151bfa93105addda72
Lakioneo <lakioneo@gmail.com>
2024-06-30 19:20:42 +0300

    V1.1

--- db_api.py
+++ db_api.py
@@ -1,60 +1,56 @@
 import sqlite3
 
-name = "data.db"
 db = None
 cursor = None
 
-#======================== DB ========================================
-def db_open(): 
-    global db,cursor
-    db = sqlite3.connect(name)
+def db_open():
+    global db, cursor
+    db = sqlite3.connect("shop.db")
     cursor = db.cursor()
-#-----------------------------------------------------------------
+    cursor.execute("""PRAGMA foreign_keys=on""")
+
 def db_close():
     db.commit()
     cursor.close()
-    db.close()    
-#-----------------------------------------------------------------
-def db_clear():
-    ''' удаляет все таблицы '''
-    db_open()
-    cursor.execute('''DROP TABLE IF EXISTS shop''')
-    cursor.execute('''DROP TABLE IF EXISTS user''')
-    cursor.execute('''DROP TABLE IF EXISTS category''')
-    db_close()
-#-----------------------------------------------------------------   
-def create():
-    db_open()
-    cursor.execute('''PRAGMA foreign_keys=on''')
+    db.close()
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS user (
+def db_create():
+    db_open()
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS user(
                         id INTEGER PRIMARY KEY,
                         name VARCHAR,
                         login VARCHAR,
                         password VARCHAR,
                         mail VARCHAR
-                            )""")
+                   )""")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS category (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS category(
                         id INTEGER PRIMARY KEY,
                         title VARCHAR
                    )""")
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS product (
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS product(
                         id INTEGER PRIMARY KEY,
                         title VARCHAR,
                         info VARCHAR,
+                        prise VARCHAR,
                         id_category INTEGER,
-                        id_user INTEGER,
-                        FOREIGN KEY (id_category) REFERENCES category(id),
-                        FOREIGN KEY (id_user) REFERENCES user(id)
+                        FOREIGN KEY (id_category) REFERENCES category(id)
                    )""")
-
-
+    
+    db_close()
+ 
+def db_del():
+    db_open()
+    cursor.execute("""DROP TABLE user""")
+    cursor.execute("""DROP TABLE product""")
+    cursor.execute("""DROP TABLE category""")
     db_close()
 
-#======================== user ========================================
+#===================================== user =====================================================
 def reg_user(name:str, login:str, password:str, mail:str):
+    """login_user -> [id]"""
     last_id = None
 
     db_open()
@@ -75,8 +71,9 @@ def reg_user(name:str, login:str, password:str, mail:str):
     db_close()
     
     return last_id
-#-----------------------------------------------------------------
+
 def login_user(login:str, password:str):
+    """login_user -> [id]"""
     db_open()
     cursor.execute("""SELECT id
                         FROM user
@@ -86,8 +83,9 @@ def login_user(login:str, password:str):
     data = cursor.fetchone()
     db_close()
     return data
-#-----------------------------------------------------------------
+
 def get_user(id:int):
+    """get_user -> [name, login, mail]"""
     db_open()
     cursor.execute("""SELECT name, login, mail
                         FROM user
@@ -96,91 +94,119 @@ def get_user(id:int):
     data = cursor.fetchone()
     db_close()
     return data
-
-#========================== category ======================================
+#===================================== category =================================================
 def add_category(title:str):
+    "return id"
     db_open()
-    cursor.execute("""INSERT INTO category(title) 
-                   VALUE (?)
+    cursor.execute("""INSERT INTO category(title)
+                            VALUES (?)  
                    """,(title,))
     last_id = cursor.lastrowid
     db_close()
     return last_id
-#-----------------------------------------------------------------
+
 def get_category(id:int):
-    """get_category -> [id, title]"""
+    "id, title"
     db_open()
     cursor.execute("""SELECT id, title
                         FROM category
-                        WHERE id == ?
+                        WHERE id == ?  
                    """,(id,))
     data = cursor.fetchone()
     db_close()
     return data
-#-----------------------------------------------------------------
+
 def get_all_category():
-    """get_all_category -> [(id, title), ...]"""
+    "id, title"
     db_open()
     cursor.execute("""SELECT id, title
                         FROM category
+                   """)
+    data = cursor.fetchall()
+    db_close()
+    return data
+#===================================== product ==================================================
+def add_product(title:str, info:str, prise:float, id_category:int):
+    "add_product -> id"
+    db_open()
+    cursor.execute("""INSERT INTO product(title,info,prise,id_category)
+                            VALUES (?,?,?,?) 
+                   """,(title,info,prise,id_category,))
+    last_id = cursor.lastrowid
+    db_close()
+    return last_id
+
+def get_product(id:int):
+    " get_product -> [id, title, info, prise, id_category] "
+    db_open()
+    cursor.execute("""SELECT id, title, info, prise, id_category
+                        FROM product
+                        WHERE id == ?  
+                   """,(id,))
+    data = cursor.fetchone()
+    db_close()
+    return data
+
+def get_all_product():
+    " get_all_product -> [(id, title, info, prise, id_category), ...] "
+    db_open()
+    cursor.execute("""SELECT id, title, info, prise, id_category
+                        FROM product
                    """)
     data = cursor.fetchall()
     db_close()
     return data
 
-#============================= product ===================================
-def add_product(title:str,info:str,id_category:int,id_user:int):
+def get_form_category_product(id_category:int):
+    "get_form_category_product -> [(id, title, info, prise, id_category), ...] "
     db_open()
-    cursor.execute("""INSERT INTO product(title,info,id_category,id_user) 
-                   VALUE (?,?,?,?)
-                   """,(title,info,id_category,id_user))
-    last_id = cursor.lastrowid
-    db_close()
-    return last_id
-#-----------------------------------------------------------------
-def get_product(id:int):
-    """get_category -> [id, title, info, id_category, id_user]"""
-    db_open()
-    cursor.execute("""SELECT id, title, info, id_category, id_user
+    cursor.execute("""SELECT *
                         FROM product
-                        WHERE id == ?
-                   """,(id,))
-    data = cursor.fetchone()
-    db_close()
-    return data
-#-----------------------------------------------------------------
-def get_all_product():
-    """get_category -> [(id, title, info, id_category, id_user), ...]"""
-    db_open()
-    cursor.execute("""SELECT id, title, info, id_category, id_user
-                        FROM product
-                   """)
-    data = cursor.fetchall()
-    db_close()
-    return data
-#-----------------------------------------------------------------
-def get_from_category_product(id_category:int):
-    """get_category -> [(id, title, info, id_category, id_user), ...]"""
-    db_open()
-    cursor.execute("""SELECT id, title, info, id_category, id_user
-                        FROM product
-                        WHERE id_category == ?
+                        WHERE id_category == ?  
                    """,(id_category,))
     data = cursor.fetchall()
     db_close()
     return data
-#================================================================
-
+#===================================== create ===================================================
 if __name__ == "__main__":
-    db_clear()
-    create()
-    
-    reg_user("nam1","log1","1111","pp1@gmail.com")
-    reg_user("name2","log2","1111","pp2@gmail.com")
-    reg_user("name3","log3","1111","pp3@gmail.com")
+    db_del()
+    db_create()
+    if True:
+        reg_user("name1", "login1", "password1", "mail1")
+        reg_user("name2", "login2", "password2", "mail2")
+        reg_user("name3", "login3", "password3", "mail3")
 
+        add_category("ТЕЛЕФОНЫ") # id = 1
+        add_category("НОУТБУКИ") # id = 2
+        add_category("МОНИТОРЫ") # id = 3
 
+        add_product("Samsung Galaxy A24", """Екран (6.5", Super AMOLED, 2340x1080) / Mediatek Helio G99 (2 x 2.6 ГГц + 6 x 2.0 ГГц) / основна потрійна камера: 50 Мп + 5 Мп + 2 Мп, фронтальна камера: 13 Мп / RAM 6 ГБ / 128 ГБ вбудованої пам'яті + microSD (до 1 ТБ) / 3G / LTE / GPS / ГЛОНАСС / BDS / підтримка 2х SIM-карток (Nano-SIM) / Android 13 / 5000 мА*год""", 7599.00, 1)
+        add_product("Apple iPhone 15 Pro Max", """Екран (6.7", OLED (Super Retina XDR), 2796x1290) / Apple A17 Pro / основна потрійна камера: 48 Мп + 12 Мп + 12 Мп, фронтальна камера: 12 Мп / 256 ГБ вбудованої пам'яті / 3G / LTE / 5G / GPS / Nano-SIM / iOS 17 """, 52999.00, 1)
+        add_product("Motorola G54 Power", """Екран (6.5", IPS, 2400x1080) / MediaTek Dimensity 7020 (2.2 ГГц + 2.0 ГГц) / подвійна основна камера: 50 Мп + 8 Мп, фронтальна камера: 16 Мп / RAM 12 ГБ / 256 ГБ вбудованої памʼяті + microSD (до 1 ТБ) / 3G / LTE / 5G / GPS / Nano-SIM + eSIM / Android 13 / 6000 мА*год""", 8888.00, 1)
 
+        add_product("ASUS TUF Gaming A15", """Екран 15.6" IPS (1920x1080) Full HD 144 Гц, матовий / AMD Ryzen 5 7535HS (3.3 - 4.55 ГГц) / RAM 16 ГБ / SSD 1 ТБ / NVIDIA GeForce RTX 3050, 4 ГБ / без ОД / Wi-Fi / Bluetooth / веб-камера / без ОС / 2.3 кг / чорний""", 34999.00, 2)
+        add_product("Apple MacBook Air 13", """Екран 13.3" Retina (2560x1600) WQXGA, глянсовий / Apple M1 / RAM 8 ГБ / SSD 256 ГБ / Apple M1 Graphics / Wi-Fi / Bluetooth / macOS Big Sur / 1.29 кг / сірий""", 31999.00, 2)
+        add_product("HP Pavilion 15-eh1106ua", """Екран 15.6" IPS (1920x1080) Full HD, матовий / AMD Ryzen 5 5500U (2.1 — 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / AMD Radeon Graphics / без ОД / Wi-Fi / Bluetooth / вебкамера / DOS / 1.75 кг / сріблястий""", 19999.00, 2)
 
+        add_product("Samsung Odyssey AG50", """Діагональ дисплея: 27"
+    Максимальна роздільна здатність дисплея: 2560x1440 (2K QHD)
+    Час реакції матриці: 1 мс (GTG)
+    Тип матриці: IPS""", 9999.00, 3)
+        add_product("Asus VG249QL3A", """Діагональ дисплея: 23.8"
+    Максимальна роздільна здатність дисплея: 1920x1080 (FullHD)
+    Час реакції матриці: 1 мс (GTG)
+    Тип матриці: IPS""", 6599.00, 3)
+        add_product("Acer EK271EBI", """Діагональ дисплея: 27"
+    Максимальна роздільна здатність дисплея: 1920x1080 (FullHD)
+    Час реакції матриці: 1 ms (VRB) / 4 ms (GTG)
+    Тип матриці: IPS""", 4499.00, 3)
+
+    print("login_user",login_user("login2","password2"))
+    print("get_user",get_user(3))
+
+    print("get_category",get_category(2))
+
+    print("get_product",get_product(3))
+    print("get_form_category_product",get_form_category_product(2))
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask  import (Flask,
+from flask import  (Flask , 
                     redirect, url_for, render_template,
                     request, session)
 
@@ -6,92 +6,108 @@ import os
 
 import db_api
 
-path = os.path.join(os.getcwd(),"Wed")
+path = os.path.join(
+                    os.getcwd(),
+                    "web"
+)
 
 app = Flask(__name__, static_folder=path, template_folder=path)
 app.config["SECRET_KEY"] = "1111"
 
 def isLogin(func):
     def body(*args, **kwargs):
-        if (not("user_id" in session) 
-                or session["user_id"] is None
-                or session["user_id"] <= 0):
-            
+        if not("user_id" in session):
             return redirect(url_for("login"))
+        if (session["user_id"] is None 
+                or session["user_id"] <= 0):
+            return redirect(url_for("login"))
+        
         return func(*args, **kwargs)
+    
     return body
 
 def inLogin(func):
     def body(*args, **kwargs):
         if "user_id" in session:
             if not(session["user_id"] is None) and session["user_id"] > 0:
-                return redirect(url_for("index"))
+                print(session["user_id"])
+                return redirect(url_for("login"))
+        
         return func(*args, **kwargs)
+    
     return body
 
 @app.route("/", 
-           endpoint = "index"
-           )
+           endpoint = "index", 
+           methods = ["POST","GET"])
 @isLogin
 def index():
     return redirect(url_for("main"))
 
 @app.route("/main", 
-           endpoint = "main"
-           )
+           endpoint = "main", 
+           methods = ["POST","GET"])
 @isLogin
 def main():
     data = db_api.get_all_product()
     user = db_api.get_user(session["user_id"])
+    print(data)
     return render_template("main.html", data = data, user = user)
 
 @app.route("/product/<id>", 
-           endpoint = "product"
-           )
+           endpoint = "product", 
+           methods = ["POST","GET"])# <a href="localhost:5000/product/{{id}}">кнопка</a>
 @isLogin
-def product(id):
+def product(id:int):
     data = db_api.get_product(id)
     user = db_api.get_user(session["user_id"])
-    return render_template("product.html", data = data, user = user)
+    return render_template("product.html", 
+                            id = data[0],
+                            title = data[1],
+                            info = data[2], 
+                            prise = data[3], 
+                            category = db_api.get_category(data[4])[1], 
+                            user = user)
 
 @app.route("/exit", 
-           endpoint = "exit"
-           )
+           endpoint = "exit", 
+           methods = ["POST","GET"])
 def exit():
     if "user_id" in session:
         del session["user_id"]
     return redirect(url_for("index"))
 
 @app.route("/login", 
-           endpoint = "login"
-           )
+           endpoint = "login", 
+           methods = ["POST","GET"])
 @inLogin
 def login():
     if request.method == "POST":
-        id = db_api.login_user(request.form.get["login"],request.form.get["password"])
+        id = db_api.login_user(request.form.get("login"),request.form.get("password")) 
         if not(id is None) and len(id) > 0:
             session["user_id"] = id[0]
             return redirect(url_for("index"))
-        
+
     return render_template("login.html")
 
 @app.route("/reg", 
-           endpoint = "reg"
-           )
+           endpoint = "reg", 
+           methods = ["POST","GET"])
 @inLogin
 def reg():
     if request.method == "POST":
-        if request.form.get["password1"] == request.form.get["password2"]:
+        if request.form.get("password1") ==  request.form.get("password2"):
             id = db_api.reg_user(
-                            request.form.get["name"],
-                            request.form.get["login"],
-                            request.form.get["password"],
-                            request.form.get["mail"]
-                            )
-            if not(id is None) and len(id) > 0:
+                request.form.get("name"),
+                request.form.get("login"),
+                request.form.get("password1"),
+                request.form.get("mail")
+            )
+            if not(id is None):
                 session["user_id"] = id[0]
                 return redirect(url_for("index"))
-        
-    return render_template("registration.html")
+
+    return render_template("reg.html")
+
 
 app.run()
